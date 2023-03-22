95f8a09be8716fc49b081db5ff7bc653bc2f4bce
Javivi-MR <javier.molinarojas@alum.uca.es>
2023-03-22 14:52:11 +0100

    Controles de datos - Falta el de horas

--- public_html/js/CambiarInfo.js
+++ public_html/js/CambiarInfo.js
@@ -8,16 +8,19 @@ $(function() {
                 $('#ImagenSala').attr('src', './images/sala1.jpg');
                 $('#inf1').html('Sala alojada en la biblioteca Luis Berenguer, San Fernando.');
                 $('#inf2').html('Aforo máximo: 10 personas.');
+                $('#Titulo').html('Sala de estudios General Serrano');
             break;
             case 'SalaDeEstudiosMaríaLeón':
                 $('#ImagenSala').attr('src', './images/sala2.jpg');
                 $('#inf1').html('Sala alojada en la Biblioteca Pública Municipal, El Puerto de Santa María.');
                 $('#inf2').html('Aforo máximo: 6 personas.');
+                $('#Titulo').html('Sala de estudios María León');
             break;
             case 'SalaDeMeetingPoint':
                 $('#ImagenSala').attr('src', './images/sala3.jpg');
                 $('#inf1').html('Sala alojada en el edificio de la juventud, Cádiz.');
                 $('#inf2').html('Aforo máximo: 20 personas.');
+                $('#Titulo').html('Sala de MeetingPoint');
             break;
         }
     });
--- public_html/js/validacion-formulario.js
+++ public_html/js/validacion-formulario.js
@@ -1,10 +1,35 @@
-$(function() {    
+function EsAlfabetico(NombreApellido)
+{
     let expresionAlfabetica = /^[a-z]+\s?[a-z]*$/i;
+    return expresionAlfabetica.test(NombreApellido) && !NombreApellido.includes('\'') && !NombreApellido.includes('\"');
+}
+
+function EsCorreo(Correo)
+{
+    let expresionCorreo = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
+    return expresionCorreo.test(Correo);
+}
+
+
+//funcion para validar numero telefonico
+function EsNumerico(Telefono)
+{
+    let expresionTelefono = /^[0-9]+$/i;
+    return expresionTelefono.test(Telefono);
+}
+
+function TieneCaracteresEsp(cadena)
+{
+    //expresion regular de caracteres validos para un correo
+    return  !cadena.includes('\'') && !cadena.includes('\"');
+}
+
+$(function ValidacionNombre() //Comprobacion de que el usuario introduzca el nombre de forma correcta (Incluye espacio para nombre compuesto Ej: Francisco Javier)
+{    
     $('#nombre').on('input', function() 
     {
-        let elem = $(this).val();
-        console.log(elem.match(expresionAlfabetica));
-        if (elem.match(expresionAlfabetica) || elem === '') 
+        let nombre = $(this).val();
+        if (EsAlfabetico(nombre) || nombre === '') 
         {
             $(this).removeClass("error");
         }
@@ -17,7 +42,61 @@ $(function() {
     });
 });
 
-$(function NumeroPersonas() {    
+$(function ValidacionApellidos() //Comprobacion de que el usuario introduzca los apellidos de forma correcta
+{    
+    $('#apellidos').on('input', function() 
+    {
+        let apellidos = $(this).val();
+        if (EsAlfabetico(apellidos) || apellidos === '') 
+        {
+            $(this).removeClass("error");
+        }
+        else
+        {
+            alert("El nombre solo admite caracteres alfabéticos (sin simbolos especiales como acentos)");
+            $(this).val(''); // Borrar el valor ingresado por el usuario
+            $(this).addClass("error"); // Agregar la clase CSS "error" al input
+        }
+    });
+});
+
+$(function ValidacionCorreo() //Comprobacion de que el usuario introduzca el correo de forma correcta
+{
+    $('#correo').on('input', function()
+    {
+        let correo = $(this).val();
+        if (TieneCaracteresEsp(correo) || correo === '')
+        {
+            $(this).removeClass("error");
+        }
+        else
+        {
+            alert("El correo introducido no es valido");
+            $(this).val(''); // Borrar el valor ingresado por el usuario
+            $(this).addClass("error"); // Agregar la clase CSS "error" al input
+        }
+    });
+});
+
+$(function ValidacionTelefono() {    //Comprobacion de que el usuario introduzca el telefono movil de forma correcta
+    $('#telefono').on('input', function() 
+    {
+        let telefono = $(this).val();
+        if (EsNumerico(telefono) || telefono === '')
+        {
+            $(this).removeClass("error");
+        }
+        else
+        {
+            alert("El nombre solo admite caracteres alfabéticos (sin simbolos especiales como acentos)");
+            $(this).val(''); // Borrar el valor ingresado por el usuario
+            $(this).addClass("error"); // Agregar la clase CSS "error" al input
+        }
+    });
+});
+
+
+$(function ValidacionNumeroPersonas() {    
     $('#NumPer').on('input', function() 
     {
         var opcionSeleccionada = $('#sala').val();
@@ -35,11 +114,12 @@ $(function NumeroPersonas() {
             break;
         }
 
-        let numPersonas = parseInt($(this).val());
+        let numPersonasRaw = $(this).val()
+        let numPersonas = parseInt(numPersonasRaw);
 
-        if (numPersonas > maxPersonas) 
+        if  (!EsNumerico(numPersonasRaw) || numPersonas > maxPersonas) 
         {
-            alert("El numero de personas seleccionado es mayor al aforo");
+            alert("Se debe seleccionar un numero de personas entre 1 y " + maxPersonas);
             $(this).val(''); // Borrar el valor ingresado por el usuario
             $(this).addClass("error"); // Agregar la clase CSS "error" al input
         }
