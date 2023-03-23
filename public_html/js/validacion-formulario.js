7d2301141dad3efaa176983e3cc77dde3771f682
Javivi-MR <javier.molinarojas@alum.uca.es>
2023-03-23 15:43:28 +0100

    Verificacion añadida tambien a contacto

--- public_html/js/validacion-formulario.js
+++ public_html/js/validacion-formulario.js
@@ -18,6 +18,13 @@ function EsNumerico(Telefono)
     return expresionTelefono.test(Telefono);
 }
 
+function sinCaracteresEspeciales(cadena)
+{
+    //debe permitir letras, numeros, espacios en blanco, acentos, comas y puntos. 
+    let expresionCaracteresEspeciales = /^[a-z0-9\sáéíóúñ.,]+$/i;
+    return expresionCaracteresEspeciales.test(cadena);
+}
+
 function AforoPermitido(numPersonas)
 {
     let aforoMaximo = 0;
@@ -143,3 +150,78 @@ function DatosValidos()
     }
     return valido;
 }
+
+function DatosValidosAyuda()
+{
+    let nombre = document.getElementById('nombre').value;
+    let Apellido = document.getElementById('apellidos').value;
+    let correo = document.getElementById('correo').value;
+    let telefono = document.getElementById('telefono').value;
+    let problema = document.getElementById('problema').value;
+
+    let cadena = "";
+    let valido = true;
+
+    if(!EsAlfabetico(nombre))
+    {
+        cadena += "-> Los nombres solo acepta carácteres alfabéticos.\n\tIntroduzcalo correctamente y vuelva a enviar el formulario.\n\n";
+        document.getElementById('nombre').classList.add('error');
+        valido = false;
+    }
+    else
+    {
+        document.getElementById('nombre').classList.remove('error');
+    }
+
+    if(!EsAlfabetico(Apellido))
+    {
+        cadena += "-> Los apellidos solo acepta carácteres alfabéticos.\n\tIntroduzcalo correctamente y vuelva a enviar el formulario.\n\n";
+        document.getElementById('apellidos').classList.add('error');
+        valido = false;
+    }
+    else
+    {
+        document.getElementById('apellidos').classList.remove('error');
+    }
+
+
+    if(!EsCorreo(correo))
+    {
+        cadena += "-> El correo no es valido.\n\tIntroduzcalo correctamente y vuelva a enviar el formulario.\n\n";
+        document.getElementById('correo').classList.add('error');
+        valido = false;
+    }
+    else
+    {
+        document.getElementById('correo').classList.remove('error');
+    }
+
+    if(!EsNumerico(telefono) || telefono.length != 9)
+    {
+        cadena += "-> El telefono debe tener 9 numeros.\n\tIntroduzcalo correctamente y vuelva a enviar el formulario.\n\n";
+        document.getElementById('telefono').classList.add('error');
+        valido = false;
+    }
+    else
+    {
+        document.getElementById('telefono').classList.remove('error');
+    }
+    if(!sinCaracteresEspeciales(problema))
+    {
+        cadena += "-> El problema no puede tener carácteres especiales.\n\tIntroduzcalo correctamente y vuelva a enviar el formulario.\n\n";
+        document.getElementById('problema').classList.add('error');
+        valido = false;
+    }
+    else
+    {
+        document.getElementById('problema').classList.remove('error');
+    }
+
+    if(!valido)
+    {
+        swal("Error,", cadena, "error");
+    }
+
+    return valido;
+}
+
